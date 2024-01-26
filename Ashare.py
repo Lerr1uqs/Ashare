54eefeebfcdb1966f90b47905378bd978dc87cb0
Lerr1uqs <624299960@qq.com>
2024-01-26 17:02:55 +0800

    complete query

--- Ashare.py
+++ Ashare.py
@@ -1,33 +1,38 @@
 import json, requests
 from datetime import datetime
 import pandas as pd
+from loguru import logger
 
 from abc import ABC, abstractmethod
 
 class ApiServerBase(ABC):
-    @abstractmethod
-    def query_daily_prices(self):
-        pass
+    # @abstractmethod
+    # def query_daily_prices(self):
+    #     pass
 
-    @abstractmethod
-    def query_hourly_prices(self):
-        pass
+    # @abstractmethod
+    # def query_hourly_prices(self):
+    #     pass
 
-    @abstractmethod
-    def query_minute_prices(self):
-        pass
+    # @abstractmethod
+    # def query_minute_prices(self):
+    #     pass
     pass
 
-# TODO: 'Xday','Xmouth', 'Xminute' # 'daily'(等同于'1d'), 'minute'(等同于'1m')
+# TODO: 'Xday','Xmonth', 'Xminute' # 'daily'(等同于'1d'), 'minute'(等同于'1m')
     
 class Tencent(ApiServerBase):
     def __init__(self) -> None:
         pass
 
-    def query_daily_prices(self, security: str, frequency="1day", end_date=datetime.now()) -> pd.DataFrame:
+    def query_prices(self, security: str, frequency="day", end_date=datetime.now(), count=10) -> pd.DataFrame:
+        '''
+        腾讯日 周 月线。
+        frequency in ["day", "week", "month"]
+        '''
 
-        if not any(frequency.endswith(f) for f in ["day", "minute", "week", "mouth"]): # TODO:
-            raise RuntimeError("frequency error")
+        if frequency not in ["day", "week", "month"]:
+            raise RuntimeError(f"frequency error : {frequency}")
 
         if not isinstance(end_date, datetime):
             raise TypeError(type(end_date))
@@ -36,15 +41,15 @@ class Tencent(ApiServerBase):
         # TODO: check security
         end_date_str = end_date.strftime(r'%Y-%m-%d')
 
-        # 提取前面的数字
-        count = int(''.join(c for c in frequency if c.isdigit()))
-        # 提取后面的单词
-        freq = ''.join(c for c in frequency if c.isalpha())
+        freq = frequency
 
         URL = f'http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param={security},{freq},,{end_date_str},{count},qfq' # TODO: qfq 前复权
 
         content = json.loads(requests.get(URL).content)
-        print(json.dumps(content, indent=4))
+
+        if content["msg"] != "":
+            raise RuntimeError(content["msg"])
+            
         '''
         {
             "code": 0,
@@ -81,89 +86,168 @@ class Tencent(ApiServerBase):
 
         return df
 
-    def query_minute_prices(self, security: str, frequency="1minute", end_date=datetime.now()) -> pd.DataFrame:
+    def query_minute_prices(self, security: str, frequency="1minute", end_date=datetime.now(), count=10) -> pd.DataFrame:
+        '''
+        腾讯分钟线.
+        frequency in [1minute, 5minute, 10minute ...]
+        '''
         if not frequency.endswith("minute"): 
             raise RuntimeError(f"frequency error :{frequency}")
+            
+        if not frequency[0].isdigit():
+            raise RuntimeError(f"frequency error {frequency}")
+            
+        if not isinstance(end_date, datetime):
+            raise TypeError(type(end_date))
+        
+        # 提取前面的数字
+        freq = int(''.join(c for c in frequency if c.isdigit()))
 
+        URL = f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={security},m{freq},,{count}' 
+
+        content = json.loads(requests.get(URL).content)
+        data = content["data"][security]["m" + str(freq)]
+
+        columns = ['time','open','close','high','low','volume','n1','n2']
+        df = pd.DataFrame(
+            data, 
+            columns=columns
+        )[columns[:-2]]
+
+        df[columns[1:-2]] = df[columns[1:-2]].astype("float")
+
+        df.loc[:, "time"] = pd.to_datetime(df["time"])
+
+        df.set_index(['time'], inplace=True)
+
+        return df
+
+class Sina(ApiServerBase):
+
+    # sina新浪全周期获取函数，分钟线 5m,15m,30m,60m
+    def query_prices(self, security: str, frequency="5m", end_date=datetime.now(), count=10) -> pd.DataFrame:
+        '''
+        frequency必须是5的倍数
+        '''
+        if not frequency.endswith("m"): 
+            raise RuntimeError(f"frequency error :{frequency}")
+            
+        if not frequency[0].isdigit():
+            raise RuntimeError(f"frequency error {frequency}")
+            
         if not isinstance(end_date, datetime):
             raise TypeError(type(end_date))
 
-    def query_hourly_prices(self):
-        pass
+        # 提取前面的数字
+        mfreq = int(''.join(c for c in frequency if c.isdigit()))
 
+        if mfreq % 5 != 0:
+            raise RuntimeError(f"frequency must be multiple of five but found :{frequency}")
 
+        URL = f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={security}&scale={mfreq}&ma=5&datalen={count}'
 
+        content = json.loads(requests.get(URL).content)
+        '''
+        这边其实还有两列数据 暂时用不上
+        "ma_price5": 29.488,
+        "ma_volume5": 331320
+        '''
+        data = content
 
-Tencent().query_daily_prices("sh000001", frequency="1day")
-Tencent().query_daily_prices("sh605577", frequency="1day")
+        columns = ['day','open','close','high','low','volume']
+        df = pd.DataFrame(
+            data, 
+            columns=columns
+        )
 
+        df[columns[1:]] = df[columns[1:]].astype("float")
+        df.loc[:, "day"] = pd.to_datetime(df["day"])
+        df.set_index(['day'], inplace=True)
 
-        
+        return df
 
-
-#腾讯日线
-def get_price_day_tx(code, end_date='', count=10, frequency='1d'):     #日线获取  
-    unit='week' if frequency in '1w' else 'month' if frequency in '1M' else 'day'     #判断日线，周线，月线
-    if end_date:  end_date=end_date.strftime('%Y-%m-%d') if isinstance(end_date,datetime.date) else end_date.split(' ')[0]
-    end_date='' if end_date==datetime.datetime.now().strftime('%Y-%m-%d') else end_date   #如果日期今天就变成空    
-    URL=f'http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param={code},{unit},,{end_date},{count},qfq'     
-    st= json.loads(requests.get(URL).content);    ms='qfq'+unit;      stk=st['data'][code]   
-    buf=stk[ms] if ms in stk else stk[unit]       #指数返回不是qfqday,是day
-    df=pd.DataFrame(buf,columns=['time','open','close','high','low','volume'],dtype='float')     
-    df.time=pd.to_datetime(df.time);    df.set_index(['time'], inplace=True);   df.index.name=''          #处理索引 
-    return df
-
-#腾讯分钟线
-def get_price_min_tx(code, end_date=None, count=10, frequency='1d'):    #分钟线获取 
-    ts=int(frequency[:-1]) if frequency[:-1].isdigit() else 1           #解析K线周期数
-    if end_date: end_date=end_date.strftime('%Y-%m-%d') if isinstance(end_date,datetime.date) else end_date.split(' ')[0]        
-    URL=f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={code},m{ts},,{count}' 
-    st= json.loads(requests.get(URL).content);       buf=st['data'][code]['m'+str(ts)] 
-    df=pd.DataFrame(buf,columns=['time','open','close','high','low','volume','n1','n2'])   
-    df=df[['time','open','close','high','low','volume']]    
-    df[['open','close','high','low','volume']]=df[['open','close','high','low','volume']].astype('float')
-    df.time=pd.to_datetime(df.time);   df.set_index(['time'], inplace=True);   df.index.name=''          #处理索引     
-    df['close'][-1]=float(st['data'][code]['qt'][code][3])                #最新基金数据是3位的
-    return df
-
-
-#sina新浪全周期获取函数，分钟线 5m,15m,30m,60m  日线1d=240m   周线1w=1200m  1月=7200m
-def get_price_sina(code, end_date='', count=10, frequency='60m'):    #新浪全周期获取函数    
-    frequency=frequency.replace('1d','240m').replace('1w','1200m').replace('1M','7200m');   mcount=count
-    ts=int(frequency[:-1]) if frequency[:-1].isdigit() else 1       #解析K线周期数
-    if (end_date!='') & (frequency in ['240m','1200m','7200m']): 
-        end_date=pd.to_datetime(end_date) if not isinstance(end_date,datetime.date) else end_date    #转换成datetime
-        unit=4 if frequency=='1200m' else 29 if frequency=='7200m' else 1    #4,29多几个数据不影响速度
-        count=count+(datetime.datetime.now()-end_date).days//unit            #结束时间到今天有多少天自然日(肯定 >交易日)        
-        #print(code,end_date,count)    
-    URL=f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale={ts}&ma=5&datalen={count}' 
-    dstr= json.loads(requests.get(URL).content);       
-    #df=pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'],dtype='float') 
-    df= pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'])
-    df['open'] = df['open'].astype(float); df['high'] = df['high'].astype(float);                          #转换数据类型
-    df['low'] = df['low'].astype(float);   df['close'] = df['close'].astype(float);  df['volume'] = df['volume'].astype(float)    
-    df.day=pd.to_datetime(df.day);    df.set_index(['day'], inplace=True);     df.index.name=''            #处理索引                 
-    if (end_date!='') & (frequency in ['240m','1200m','7200m']): return df[df.index<=end_date][-mcount:]   #日线带结束时间先返回              
-    return df
-
-def get_price(code, end_date='',count=10, frequency='1d', fields=[]):        #对外暴露只有唯一函数，这样对用户才是最友好的  
-    xcode= code.replace('.XSHG','').replace('.XSHE','')                      #证券代码编码兼容处理 
-    xcode='sh'+xcode if ('XSHG' in code)  else  'sz'+xcode  if ('XSHE' in code)  else code     
-
-    if  frequency in ['1d','1w','1M']:   #1d日线  1w周线  1M月线
-         try:    return get_price_sina( xcode, end_date=end_date,count=count,frequency=frequency)   #主力
-         except: return get_price_day_tx(xcode,end_date=end_date,count=count,frequency=frequency)   #备用                    
+class Api:
+    def __init__(self) -> None:
+        self._tencent = Tencent()
+        self._sina = Sina()
     
-    if  frequency in ['1m','5m','15m','30m','60m']:  #分钟线 ,1m只有腾讯接口  5分钟5m   60分钟60m
-         if frequency in '1m': return get_price_min_tx(xcode,end_date=end_date,count=count,frequency=frequency)
-         try:    return get_price_sina(  xcode,end_date=end_date,count=count,frequency=frequency)   #主力   
-         except: return get_price_min_tx(xcode,end_date=end_date,count=count,frequency=frequency)   #备用
-        
-# if __name__ == '__main__':    
-#     df=get_price('sh000001',frequency='1d',count=10)      #支持'1d'日, '1w'周, '1M'月  
-#     print('上证指数日线行情\n',df)
-    
-#     df=get_price('000001.XSHG',frequency='15m',count=10)  #支持'1m','5m','15m','30m','60m'
-#     print('上证指数分钟线\n',df)
+    def query_prices_untilnow(self, security: str, frequency='60minute', count=10) -> pd.DataFrame:
+        '''
+        tx支持: 1minute 5minute 10minute... 1day 1week 1month
+        xl支持:         5minute 10minute... 1day 1week 1month
+        '''
 
-# Ashare 股票行情数据( https://github.com/mpquant/Ashare ) 
+        while True:
+            if any(security.endswith(end) for end in [".XSHG", ".XSHE"]):
+                break
+            
+            if any(security.startswith(start) for start in ["sh", "sz"]):
+                break
+            
+            raise RuntimeError(f"security format error : {security}")
+
+        n    = int(''.join(c for c in frequency if c.isdigit()))
+        freq = ''.join(c for c in frequency if c.isalpha())
+        
+        if freq not in ["minute", "day", "week", "month"]:
+            raise RuntimeError(f"frequency error : {frequency}")
+        
+        #证券代码编码兼容处理 
+        code = security.replace('.XSHG', '').replace('.XSHE', '')
+        code = 'sh' + code if ('XSHG' in security) else 'sz' + code if ('XSHE' in security) else security
+
+        if freq == "minute":
+
+            try:
+                return self._tencent.query_minute_prices(security, frequency, count=count)
+            except Exception as e:
+                logger.info(f"found exception {e}, try next server")
+                
+            try:
+                return self._sina.query_prices(security, frequency=str(n)+"m", count=count)
+            except Exception as e:
+                logger.error(f"backup server failed with {e}")
+                raise e
+
+        elif freq in ["day", "week", "month"]:
+
+            if n != 1:
+                raise RuntimeError("only support 1 day/week/month")
+
+            try:
+                return self._tencent.query_prices(security, frequency=freq, count=count)
+            except Exception as e:
+                logger.info(f"found exception {e}, try next server")
+                
+            try:
+                # 日线1d=240m   周线1w=1200m  1月=7200m
+                if freq == "1day":
+                    freq = "240m"
+
+                elif freq == "1week":
+                    freq = "1200m"
+                    
+                elif freq == "1month":
+                    freq = "7200m"
+
+                else:
+                    raise RuntimeError(f"unhandled {freq}")
+                
+                return self._sina.query_prices(security, frequency=freq, count=count)
+
+            except Exception as e:
+                logger.error(f"backup server failed with {e}")
+                raise e
+
+        else:
+            raise RuntimeError(f"unhandled freq : {freq}")
+
+api = Api()
+            
+
+if __name__ == "__main__":
+    print(api.query_prices_untilnow("sh605577", "1minute", count=3))
+    print(api.query_prices_untilnow("sh605577", "5minute", count=3))
+    print(api.query_prices_untilnow("sh605577", "1day", count=3))
+    print(api.query_prices_untilnow("sh605577", "1week", count=3))
+    print(api.query_prices_untilnow("sh605577", "1month", count=3))
--- test.py
+++ test.py
@@ -2,12 +2,25 @@ import json, requests
 from datetime import datetime
 
 code="sh000001"
-unit="day"
-start_date=datetime(2021, 5, 27).strftime('%Y-%m-%d')
+unit="week"
+# start_date=datetime(2021, 5, 27).strftime('%Y-%m-%d')
 end_date=datetime.now().strftime('%Y-%m-%d')
-count=100
+count=5
 URL=f'http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param={code},{unit},,{end_date},{count},qfq'     
-st=json.loads(requests.get(URL).content)
-print(json.dumps(st, indent=4))
+# st=json.loads(requests.get(URL).content)
+# print(json.dumps(st, indent=4))
 # ms='qfq'+unit
 # stk=st['data'][code]   
+
+ts=5
+count=5
+URL=f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={code},m{ts},,{count}' 
+st=json.loads(requests.get(URL).content)
+print(json.dumps(st, indent=4))
+
+ts=5 # 必须是5的倍数
+code="sh000001"
+count = 10
+URL=f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale={ts}&ma=5&datalen={count}' 
+# st=json.loads(requests.get(URL).content)
+# print(json.dumps(st, indent=4))
